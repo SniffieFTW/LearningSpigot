64ecea6807a5a2da5a64a6a74d9a208f1752f3a4
WhiteTiger Development <brodycole929@gmail.com>
2023-01-17 02:38:16 -0600

    Made it Possible to have Multiple way so Spawning FireWorks

--- src/main/java/com/hill/messing/messingaround/Heart/FWork.java
+++ src/main/java/com/hill/messing/messingaround/Heart/FWork.java
@@ -4,13 +4,18 @@ import org.bukkit.FireworkEffect;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.util.BlockIterator;
 import org.bukkit.util.Vector;
+
+import java.util.Set;
 
 public class FWork implements CommandExecutor {
 
@@ -20,11 +25,19 @@ public class FWork implements CommandExecutor {
         if (sender instanceof Player){
             //Makes sure it's a player doing the command
             Player p = (Player) sender;
+            // Gets the Block The player is looking at
+
+            Block b = p.getTargetBlock((Set<Material>) null, 5);
             //Gets PLayers Location and Direction
-            Location loc = p.getEyeLocation();
-            Vector dir = p.getLocation().getDirection().multiply(5);
+            Location loc = b.getLocation();
+
+            //If you just want to do a distance and not a block use Vector and set loc = p.getLocation() and set front = loc.add(dir)
+                // & Commit out Block b = p...
+
+            //Vector dir = p.getLocation().getDirection().multiply(10);
+
             // Combines the Players Location and Direction
-            Location front = loc.add(dir);
+            Location front = loc;
             // FireWork Variable
             Firework firework = p.getPlayer().getWorld().spawn(front, Firework.class);
             // FireWork Meta
